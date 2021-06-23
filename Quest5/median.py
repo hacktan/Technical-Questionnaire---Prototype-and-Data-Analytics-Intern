b146ebf5e332ea017880b246354676b0d39e6c7d
Haktan <haktankocyigit@gmail.com>
2021-06-23 17:36:07 +0300

    Update median.py

--- Quest5/median.py
+++ Quest5/median.py
@@ -6,7 +6,7 @@ Created on Tue Jun 22 16:14:48 2021
 """
 #%% Question 5
 import pandas as pd
-df = pd.read_csv("country_vaccination_stats.csv", delimiter=",")
+df = pd.read_csv(r"http://pi.works/country_vaccinations_dataset", delimiter=",")
 df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y')
 
 medians=df.groupby('country').median().sort_values(by=['daily_vaccinations'],ascending=False)
